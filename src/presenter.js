1489a5abb5ee382d2543a5fdc28c4c0892a5810e
AndrePruden <andrepruden0@gmail.com>
2024-02-08 09:41:42 -0400

    Corregido

--- src/presenter.js
+++ src/presenter.js
@@ -2,7 +2,9 @@ import multiplicar from "./multiplicador";
 import sumar from "./sumador";
 
 const first = document.querySelector("#primer-numero");
+const primero = document.querySelector("#primero-num");
 const second = document.querySelector("#segundo-numero");
+const segundo = document.querySelector("#segundo-num");
 const form = document.querySelector("#sumar-form");
 const form2 = document.querySelector("#multiplicar-form");
 const div = document.querySelector("#resultado-div");
@@ -19,8 +21,8 @@ form.addEventListener("submit", (event) => {
 form2.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const firstNumber = Number.parseInt(primero.value);
+  const secondNumber = Number.parseInt(segundo.value);
 
   div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
